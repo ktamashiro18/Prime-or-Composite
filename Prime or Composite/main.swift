a33fa996331180330b23becc8ef60f7d01868a8c
ktamashiro18 <Kelli@kellis-macbook-air.local>
2016-01-20 08:41:45 -1000

    Initial Commit

---
+++ Prime or Composite/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Prime or Composite
+//
+//  Created by Kelli Tamashiro on 1/20/16.
+//  Copyright © 2016 Kelli Tamashiro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
